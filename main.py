10f8bda9de2dfeeb35b9a20ea8d263f1b6cc6bd3
Norbert Gałązka <norbert1996galazka@gmail.com>
2024-01-04 13:48:15 +0100

    add try except to get_temp func, and while loop to main

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ import requests
 
 def get_weather_site_html(url):
     response = requests.get(url)
+
     if response.status_code == 200:
         return BeautifulSoup(response.text, 'html.parser')
-    return None
 
 
 def get_temperature(soup) -> str:
-    bs4_element = soup.find('span', {'class': "air-temp"})
-    temperature = str(bs4_element.text)
-    return temperature.strip()
+    try:
+        bs4_element = soup.find('span', {'class': "air-temp"})
+        temperature = str(bs4_element.text)
+        return temperature.strip()
+    except AttributeError:
+        print('Niestety, nie ma takiej miejscowości')
 
 
 def main():
-    city = input('Dla jakiej miejscowości chcesz sprawdzić pogodę?: ')
-    city_with_dash = city.replace(' ', '-')
+    again = 'Wpisz miasto'
+    while True:
+        city = input(f'{again}, dla którego chcesz sprawdzić pogodę: ')
+        city_with_dash = city.replace(' ', '-')
 
-    adress = f'https://www.pogodairadar.pl/pogoda/{city_with_dash}'
-
-    return f'\nW miejscowości {city.title()} aktualna temperatura to' \
-           f' {get_temperature(get_weather_site_html(adress))} Celsjusza'
+        adress = f'https://www.pogodairadar.pl/pogoda/{city_with_dash}'
+        temperature = get_temperature(get_weather_site_html(adress))
+        if temperature:
+            return f'\nW miejscowości {city.title()} aktualna temperatura to' \
+                f' {get_temperature(get_weather_site_html(adress))} Celsjusza'
+        else:
+            again = '\nSpróbuj ponownie wpisać miasto'
+            continue
 
 
 if __name__ == '__main__':
